cb86019042d07f19c1854d39c4d1644727cc4f74
papadkostas <kostas.papadopulos@gmail.com>
2014-06-25 10:45:40 +0300

    Update RDA5807M.h

--- RDA5807M/RDA5807M.h
+++ RDA5807M/RDA5807M.h
@@ -37,29 +37,31 @@ int RDA5807_Channel(void);
 // Definitions
 //--------------------------------------------------------------
 /* I2C Address */
-#define RDASequential 	0x10
-#define RDARandom  		0x11
-#define TEA5767Mode		0x60
+#define RDASequential 	0x10  //Current Working Communication
+#define RDARandom  		  0x11  //To Do Soon
+#define TEA5767Mode		  0x60  //Not implemented
+
 /* Initialization Options */
-#define StartingFreq 	87.000
-#define EndingFreq 		108.00
-#define DefaultFreq		87.000
+#define StartingFreq 	  87.000
+#define EndingFreq 		  108.00
+#define DefaultFreq		  87.000
 #define StartingVolume 	0x4
+
 /* RDA5807M Function Code  */
 /* Register 0x00H (16Bits) */
 #define RDA_CHIP_ID     0x0058
 /* Register 0x02H (16Bits) */
 #define RDA_DHIZ        0x8000
-#define RDA_MUTE		0x8000
+#define RDA_MUTE		    0x8000
 #define RDA_MONO_ON     0x2000
-#define RDA_MONO_OFF	0xDFFF
+#define RDA_MONO_OFF	  0xDFFF
 #define RDA_BASS_ON     0x1000
-#define RDA_BASS_OFF	0xEFFF
+#define RDA_BASS_OFF	  0xEFFF
 #define RDA_RCLK_MODE   0x0800
 #define RDA_RCLK_DIRECT 0x0400
 #define RDA_SEEK_UP     0x0300
-#define RDA_SEEK_DOWN	0x0100
-#define RDA_SEEK_STOP	0xFCFF
+#define RDA_SEEK_DOWN	  0x0100
+#define RDA_SEEK_STOP	  0xFCFF
 #define RDA_SEEK_WRAP   0x0080
 #define RDA_SEEK_NOWRAP 0xFF7F
 #define RDA_CLK_0327    0x0000
@@ -70,13 +72,13 @@ int RDA5807_Channel(void);
 #define RDA_CLK_1920    0x0030
 #define RDA_CLK_3840    0x0070
 #define RDA_RDS_ON      0x0008
-#define RDA_RDS_OFF		0xFFF7
+#define RDA_RDS_OFF		  0xFFF7
 #define RDA_NEW         0x0004
 #define RDA_RESET       0x0002
 #define RDA_POWER       0x0001
 /* Register 0x03H (16Bits) */
-#define RDA_TUNE_ON		0x0010
-#define RDA_TUNE_OFF	0xFFEF
+#define RDA_TUNE_ON		  0x0010
+#define RDA_TUNE_OFF	  0xFFEF
 //--------------------------------------------------------------
 // Global Variables
 //--------------------------------------------------------------
