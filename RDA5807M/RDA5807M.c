d5b682b022504f6551906e48b5e07ba05900f756
Paul Staron <paulstaron@hotmail.co.uk>
2015-12-26 19:24:08 +0000

    Update RDA5807M.c
    Some RDA5807M i2c changes, added RDS Station Name, RDS Text, RDS CT time display functions.

--- RDA5807M/RDA5807M.c
+++ RDA5807M/RDA5807M.c
@@ -1,4 +1,5 @@
 #include "RDA5807M.h"
+#include <string.h>
 //--------------------------------------------------------------
 // Local Variables
 //--------------------------------------------------------------
@@ -170,33 +171,166 @@ int8_t RDA5807_RDS(){
     return RDAstatus = RDA5807_WriteAll();
 }
 
-int8_t RDA5807_Read(){
+int8_t RDA5807_RDSinit(){
+  strcpy(StationName, "--------");
+  strcpy(PSName, "        ");
+  strcpy(PSName1, "        ");
+  strcpy(PSName2, "        ");
+  memset(RDStext, '\0', sizeof(RDStext));
+  memset(RDStextbuffer, '\0', sizeof(RDStextbuffer));
+  lastTextIDX = 0;
+  mins=0;
+  sprintf(CTtime, "CT --:--");
+  return RDAstatus;
+}
+
+int8_t RDA5807_Read(){  // Read all RDA5807 registers
 	UB_Led_On(LED_GREEN);
-	int8_t i,x=0;
+	int8_t i;
 	RDAstatus = UB_I2C3_ReadMultiByte(RDASequential<<1,0x00,14,0);
-	for(i=0; i<13; i=i+2){
-		RDA5807M_ReadReg[x] = I2C3_DATA[i];
-		RDA5807M_ReadReg[x] = RDA5807M_ReadReg[x] << 8;
-		x++;
+	for(i=0; i<6; i++){
+		RDA5807M_ReadReg[i] = ((I2C3_DATA[i *2] << 8) | I2C3_DATA [(i*2) +1] );
 	}
-	x = 0;
-	for(i=1; i<14; i=i+2){
-		RDA5807M_ReadReg[x] = RDA5807M_ReadReg[x] | I2C3_DATA[i];
-		x++;
-	}
+
+	block1 = RDA5807M_ReadReg[2];  //RDS Text data blocks
+	block2 = RDA5807M_ReadReg[3];
+	block3 = RDA5807M_ReadReg[4];
+	block4 = RDA5807M_ReadReg[5];
+
 	rdsready = RDA5807M_ReadReg[0] & 0x8000;			//if rdsready != 0 rds data are ready
 	tuneok = RDA5807M_ReadReg[0] & 0x4000;				//if tuneok != 0 seek/tune completed
 	nochannel = RDA5807M_ReadReg[0] & 0x2000;			//if nochannel != 0 no channel found
 	rdssynchro = RDA5807M_ReadReg[0] & 0x1000;			//if rdssynchro = 1000 rds decoder syncrhonized
 	stereo = RDA5807M_ReadReg[0] & 0x0400; 				//if stereo = 0 station is mono else stereo
 	freq = (((RDA5807M_ReadReg[0] & 0x03FF) * 100) + 87000);	//return freq ex 102600KHz > 102.6MHz
-	signal = RDA5807M_ReadReg[1] >> 10;				//return signal strength rssi
+	signal = RDA5807M_ReadReg[1] >> 10;					//return signal strength rssi
 	fmready = RDA5807M_ReadReg[1] & 0x0008; 			//if fmready = 8 > fm is ready
 	fmstation = RDA5807M_ReadReg[1] & 0x0100; 			//if fmstation = 100 fm station is true
-	rdsblockerror = RDA5807M_ReadReg[1] & 0x000C;			//check for rds blocks errors
-																//00= 0 errors,01= 1~2 errors requiring correction
-																//10= 3~5 errors requiring correction
-																//11= 6+ errors or error in checkword, correction not possible.
+	rdsblockerror = RDA5807M_ReadReg[1] & 0x000C;		//check for rds blocks errors
+														//00= 0 errors,01= 1~2 errors requiring correction
+														//10= 3~5 errors requiring correction
+														//11= 6+ errors or error in checkword, correction not possible.
+
+	if (rdssynchro != 0x1000){  // RDS not synchronised or tuning changed, reset all the RDS info.
+		RDA5807_RDSinit();
+	}
+	// analyzing Block 2
+	rdsGroupType = 0x0A | ((block2 & 0xF000) >> 8) | ((block2 & 0x0800) >> 11);
+	rdsTP = (block2 & 0x0400);
+	rdsPTY = (block2 & 0x0400);
+	    switch (rdsGroupType) {
+	        case 0x0A:
+	        case 0x0B:
+	        // The data received is part of the Service Station Name
+	        idx = 2 * (block2 & 0x0003);
+	        // new data is 2 chars from block 4
+	        c1 = block4 >> 8;
+	        c2 = block4 & 0x00FF;
+	        // check that the data was received successfully twice
+	        // before sending the station name
+	        if ((PSName1[idx] == c1) && (PSName1[idx + 1] == c2)) {
+	            // retrieve the text a second time: store to _PSName2
+	            PSName2[idx] = c1;
+	            PSName2[idx + 1] = c2;
+	            PSName2[8] = '\0';
+	            if (strcmp(PSName1, PSName2) == 0) {
+	                // populate station name
+	            	int8_t n=0;
+	                for(i=0;i<(8);i++){ // remove non-printable error ASCCi characters
+	                    if(PSName2[i] > 31 && PSName2[i] < 127){
+	                        StationName[n] = PSName2[i];
+	                        n++;
+	                    }
+	                }
+	            }
+	        }
+	        if ((PSName1[idx] != c1) || (PSName1[idx + 1] != c2)) {
+	            PSName1[idx] = c1;
+	            PSName1[idx + 1] = c2;
+	            PSName1[8] = '\0';
+	        }
+	    break;
+
+	    case 0x2A:
+// RDS text
+	    textAB = (block2 & 0x0010);
+	    idx = 4 * (block2 & 0x000F);
+	    if (idx < lastTextIDX) {
+	      // The existing text might be complete because the index is starting at the beginning again.
+	      // Populate RDS text array.
+	    	int8_t n=0;
+	        for(i=0;i<strlen(RDStextbuffer);i++){
+	            if(RDStextbuffer[i] > 31 && RDStextbuffer[i] < 127){    // remove non printable error characters
+	                RDStext[n] = RDStextbuffer[i];
+	                n++;
+	            }
+	        }
+	      }
+	    lastTextIDX = idx;
+	    if (textAB != lasttextAB) {
+	      // when this bit is toggled text data has changed, the whole buffer should be cleared.
+	      lasttextAB = textAB;
+	      if(signal>35){  // clear buffer if higher signal level to prevent spurious text blanking with error characters.
+	    	  memset(RDStextbuffer, 0, sizeof(RDStextbuffer));
+	      	  memset(RDStext, '\0', sizeof(RDStext));
+	      }
+	    }
+	    if(rdsblockerror < 4){ // limit RDS data errors as we have no correction code
+	        // new data is 2 chars from block 3
+	    	RDStextbuffer[idx] = (block3 >> 8);     idx++;
+	    	RDStextbuffer[idx] = (block3 & 0x00FF); idx++;
+	        // new data is 2 chars from block 4
+	    	RDStextbuffer[idx] = (block4 >> 8); idx++;
+	    	RDStextbuffer[idx] = (block4 & 0x00FF); idx++;
+	    }
+	    break;
+
+	  case 0x4A:
+// Clock time and date
+	    if(rdsblockerror <3){ // limit RDS data errors as we have no correction code
+	        offset = (block4) & 0x3F; // 6 bits
+	        mins = (block4 >> 6) & 0x3F; // 6 bits
+	        mins += 60 * (((block3 & 0x0001) << 4) | ((block4 >> 12) & 0x0F));
+	    }
+	    // adjust offset
+	    if (offset & 0x20) {
+	      mins -= 30 * (offset & 0x1F);
+	    }
+	    else {
+	      mins += 30 * (offset & 0x1F);
+	    }
+
+	    if(mins>0 && mins<1500){
+	    	sprintf(CTtime, "CT %2d:%02d",(mins/60),(mins%60));  // CT time formatted string
+	    }
+
+	    break;
+
+	  case 0x6A:
+	    // IH
+	    break;
+
+	  case 0x8A:
+	    // TMC
+	    break;
+
+	  case 0xAA:
+	    // TMC
+	    break;
+
+	  case 0xCA:
+	    // TMC
+	    break;
+
+	  case 0xEA:
+	    // IH
+	    break;
+
+	  default:
+
+	    break;
+	  }
+
 	UB_Led_Off(LED_GREEN);
 	return RDAstatus;
 }
